c04792771126fe08a95c1e6c6c2be5eb547dc96f
Mariá Calmon Dombrate <mari.dombrate@hotmail.com>
2022-08-31 14:47:31 -0300

    Login criado

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/static/Footer/Footer';
 import Navbar from './components/static/Navbar/Navbar';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <>
+    <Router>
       
       <Navbar />
-      {/*<div style={{ minHeight: '100vh' }}>
-        <Routes> // Antigo Switch
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> 
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/cadastro" element={<CadastroUsuario />} />
+          {/* <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} /> */}
         </Routes>
-  </div>*/}
+  </div>
       <Footer />
-    </>
+    </Router>
   )
 }
 
