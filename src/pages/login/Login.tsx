2f007744a2345c39c34aba6f54ee92ccb9c1347e
Ingrid Aquino <ingridaquinosoares@live.com>
2022-09-12 09:37:32 -0300

    :construction: Criado as pastas de posts e temas

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -59,9 +59,9 @@ function Login() {
             <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
             <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
             <Box marginTop={2} textAlign='center'>
-              <Link to='/home' className='text-decorator-none'>
+              {/* <Link to='/home' className='text-decorator-none'> */}
                 <Button type='submit' variant='contained' color='primary' className='btn'>Logar</Button>
-              </Link>
+              {/* </Link> */}
             </Box>
           </form>
           <Box display='flex' justifyContent='center' marginTop={2}>
@@ -82,7 +82,6 @@ function Login() {
         backgroundSize: 'cover',
         backgroundPosition: 'center'
       }}>
-
       </Grid>
     </Grid>
   );
