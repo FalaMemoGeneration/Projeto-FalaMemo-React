62e511fdd60bd726c544241b91c427f76d47dfbc
Mariá Calmon Dombrate <mari.dombrate@hotmail.com>
2022-09-12 11:36:12 -0300

    Deletar tema criado

--- src/components/temas/deletarTema/DeletarTema.tsx
+++ src/components/temas/deletarTema/DeletarTema.tsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core';
+import { Box } from '@mui/material';
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import Tema from '../../../models/Tema';
+import { buscaId, deleteId } from '../../../services/Service';
+import { TokenState } from '../../../store/tokens/tokensReducer';
 
 function DeletarTema() {
-  return (
-    <div>DeletarTema</div>
-  )
-}
+  let navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state) => state.tokens
+  );
+  const [tema, setTema] = useState<Tema>({
+    id: 0,
+    descricao: "",
+  });
 
+  useEffect(() => {
+    if (token === "") {
+      alert("Você precisa estar logado!");
+      navigate("/login");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (id !== undefined) {
+      findById(id);
+    }
+  }, [id]);
+
+  async function findById(id: string) {
+    buscaId(`/tema/${id}`, setTema, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  function sim() {
+    navigate('/temas')
+    deleteId(`/temas/${id}`, {
+      headers: {
+        'Authorization': token
+      }
+    });
+    alert('Tema deletado com sucesso.');
+  }
+
+  function nao() {
+    navigate('/temas')
+  }
+
+  return (
+    <>
+      <Box m={2}>
+        <Card variant="outlined">
+          <CardContent>
+            <Box justifyContent="center">
+              <Typography color="textSecondary" gutterBottom>
+                Deseja deletar o tema?
+              </Typography>
+              <Typography color="textSecondary">
+                {tema?.descricao}
+              </Typography>
+            </Box>
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="start" ml={1.0} mb={2} >
+              <Box mx={2}>
+                <Button onClick={sim} variant="contained" className="marginLeft btnCancelar" color="primary">
+                  Sim
+                </Button>
+              </Box>
+              <Box mx={2}>
+                <Button onClick={nao} variant="contained" color="secondary" className='btn'>
+                  Não
+                </Button>
+              </Box>
+            </Box>
+          </CardActions>
+        </Card>
+      </Box>
+    </>
+  );
+}
 export default DeletarTema
