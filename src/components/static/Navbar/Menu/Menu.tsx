4a1b66a6f3e6c8f884ebdd8ea89edf2c2ee56a67
Amanda Falcão <amandaf.dias96@gmail.com>
2022-09-13 15:56:18 -0300

    Menu hamburguer adicionado

---
+++ src/components/static/Navbar/Menu/Menu.tsx
@@ -0,0 +1,71 @@
+import "./Menu.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToken } from "../../../../store/tokens/actions";
+
+export default function BasicMenu() {
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+
+  function changeClick() {
+    handleClose();
+    goLogout();
+  }
+
+  function goLogout() {
+    dispatch(addToken(""));
+    alert("Você saiu!");
+    navigate("/login");
+  }
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </Button>
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link to="/home" className="text-decorator-none">
+            Home
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/postagens" className="text-decorator-none">
+            Experiências
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={changeClick}>Sair</MenuItem>
+      </Menu>
+    </div>
+  );
+}
