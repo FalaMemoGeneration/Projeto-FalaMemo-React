0cd70e7a3efa236e9394a337d74b3bde9231dc33
Ingrid Aquino <ingridaquinosoares@live.com>
2022-09-17 14:39:27 -0300

    :building_construction: Empresa e experiencias estilizados

--- src/components/temas/listarTema/ListarTema.tsx
+++ src/components/temas/listarTema/ListarTema.tsx
@@ -1,19 +1,27 @@
-import { Button, Card, CardActions, CardContent, Typography } from "@material-ui/core"
-import { Box } from "@mui/material"
-import React, { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-import { Link, useNavigate } from "react-router-dom"
-import { toast } from "react-toastify"
-import Tema from "../../../models/Tema"
-import { busca } from "../../../services/Service"
-import { TokenState } from "../../../store/tokens/tokensReducer"
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Tema from "../../../models/Tema";
+import { busca } from "../../../services/Service";
+import { TokenState } from "../../../store/tokens/tokensReducer";
+
+import "./ListarTema.css";
 
 function ListaTema() {
-  const [temas, setTemas] = useState<Tema[]>([])
+  const [temas, setTemas] = useState<Tema[]>([]);
   const token = useSelector<TokenState, TokenState["tokens"]>(
     (state) => state.tokens
-  )
-  let navigate = useNavigate()
+  );
+  let navigate = useNavigate();
 
   useEffect(() => {
     if (token == "") {
@@ -24,70 +32,73 @@ function ListaTema() {
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
-        progress: undefined
-      })
-      navigate("/login")
+        progress: undefined,
+      });
+      navigate("/login");
     }
-  }, [token])
+  }, [token]);
 
   async function getTema() {
     await busca("/temas", setTemas, {
       headers: {
         Authorization: token,
       },
-    })
+    });
   }
 
   useEffect(() => {
-    getTema()
-  }, [temas.length])
+    getTema();
+  }, [temas.length]);
   return (
     <>
-      {
-        temas.map(tema => (
-          <Box m={2} key={tema.id}>
-            <Card variant="outlined">
-              <CardContent>
-                <Typography color="textSecondary" gutterBottom>
-                  Tema
-                </Typography>
-                <Typography variant="h5" component="h2" className="color-weight">
-                  {tema.descricao}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Box display="flex" justifyContent="center" mb={1.5}>
-                  <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
-                    <Box mx={1}>
-                      <Button
-                        variant="contained"
-                        className="marginLeft btn"
-                        size="small"
-                        color="primary"
-                      >
-                        atualizar
-                      </Button>
-                    </Box>
-                  </Link>
-                  <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
-                    <Box mx={1}>
-                      <Button
-                        variant="contained"
-                        size="small"
-                        color="secondary"
-                        className="btnCancelar"
-                      >
-                        deletar
-                      </Button>
-                    </Box>
-                  </Link>
-                </Box>
-              </CardActions>
-            </Card>
-          </Box>
-        ))}
+      <Typography variant="h3" component="h3" className="experiencias">
+        Empresas
+      </Typography>
+      <div className="publi">
+      {temas.map((tema) => (
+        <div key={tema.id} className="box-empresa">
+          <div className="caixa-empresa">
+            <CardContent >
+              <Typography variant="h5" component="h5" className="empresa02">
+                {tema.descricao}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Box display="flex" justifyContent="center" mb={1.5}>
+                <Link
+                  to={`/formularioTema/${tema.id}`}
+                  className="text-decorator-none"
+                >
+                  <Box mx={1}>
+                    <Button
+                      variant="contained"
+                      className="marginLeft btnAtuali"
+                    >
+                      atualizar
+                    </Button>
+                  </Box>
+                </Link>
+                <Link
+                  to={`/deletarTema/${tema.id}`}
+                  className="text-decorator-none"
+                >
+                  <Box mx={1}>
+                    <Button
+                      variant="contained"
+                      className="btnDeletar"
+                    >
+                      deletar
+                    </Button>
+                  </Box>
+                </Link>
+              </Box>
+            </CardActions>
+          </div>
+        </div>
+      ))}  
+    </div>
     </>
-  )
+  );
 }
 
-export default ListaTema
+export default ListaTema;
